af3ad30b273b862f9566c047ad76b0f02018bb62
rohan saraf <rohan.saraf@oyorooms.com>
2022-05-18 02:21:15 +0530

    added trading context

--- config.py
+++ config.py
@@ -3,3 +3,14 @@ import os
 API_KEY = os.getenv("ANGEL_API_KEY", "2EuTpdkW")
 CLIENT_ID = os.getenv("ANGEL_CLIENT_ID", "R232944")
 PASSWORD = os.getenv("ANGEL_PASSWORD", "jagadamba951")
+
+POSITION_STATE_MACHINE = {
+    "open": "close_requested",
+    "close_requested": "closed"
+}
+
+ORDER_DETAIL_STATE_MACHINE =    {
+    "open_requested": ["opened", "cancel_requested"],
+    "opened": ["filled", "cancel_requested"],
+    "cancel_requested": ["cancelled"]
+}
--- core/trading_context.py
+++ core/trading_context.py
@@ -1,9 +1,20 @@
 class TradingContext:
-    def __init__(self, order_details={}, positions={}, trades={}) -> None:
-        self.order_details = order_details
-        self.positions = positions
-        self.trades = trades
+    def __init__(self, order_details={}, positions={}, trades={}, position_mapping={}) -> None:
+        self.order_details = order_details # placed orders
+        self.positions = positions # executed orders
+        self.trades = trades # each executed order is a trades
 
     def update_order_info(self, order_detail):
-        if order_detail.type == "BUY":
-            self.order_details.update(order_detail.id, order_detail)
+        if order_detail.order_id in self.order_details:
+            existing_order_detail = self.order_details[order_detail.order_id]
+            existing_order_detail.history.append("state transition history")
+            existing_order_detail.status = order_detail.status
+            self.order_details[order_detail.order_id] = existing_order_detail
+        else:
+            self.order_details[order_detail.order_id] = order_detail
+
+    
+    def update_order_position(self, order_detail):
+        # if there is any position for the order_id
+        if order_detail.id in self.positions:
+            pass
